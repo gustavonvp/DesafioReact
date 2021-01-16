371813b1d7e25a56f5926d71a7b5f025a0ff42bd
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2021-01-15 21:18:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import { render } from 'react-dom'
 
 import App from './App'
 
-render(<App />,document.getElementById('app'))
+render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('app')
+);
 
